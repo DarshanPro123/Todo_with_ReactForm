5f2d1117a07bf88831cdc0ff319897a6f073ae16
Darshanpro123 <darshanpro187@gmail.com>
2024-10-16 11:55:47 +0530

    add tasks in localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { ToastContainer, toast, Slide } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+const getData = () => {
+  const data = localStorage.getItem("Todos");
+
+  if (data) return JSON.parse(data);
+  return [];
+};
 
 function App() {
   const {
@@ -11,7 +18,7 @@ function App() {
     reset,
   } = useForm();
 
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(getData());
 
   const addTask = (task) => {
     setTasks([...tasks, { id: Date.now(), todo: task.todo }]);
@@ -29,8 +36,12 @@ function App() {
     toast.error(`${todo} delete Sucessfully`);
   };
 
+  useEffect(() => {
+    localStorage.setItem("Todos", JSON.stringify(tasks));
+  }, [tasks]);
+
   return (
-    <div className="h-screen bg-gradient-to-r from-[#1F357B] via-[#30286E] to-[#3E1D6A] flex items-center justify-center">
+    <div className="h-screen bg-gradient-to-r from-[#1F357B] via-[#42369d] to-[#3E1D6A] flex items-center justify-center">
       <div className="w-10/12 md:w-1/2 lg:w-1/3 h-auto p-10 mt-10 text-center mx-auto bg-gray-50 rounded-lg shadow-lg">
         <h1 className="text-3xl font-bold text-gray-800 mb-4">
           My Todo App ✅{" "}
@@ -39,14 +50,14 @@ function App() {
         <form onSubmit={handleSubmit(onSubmit)}>
           <input
             {...register("todo", { required: true })}
-            className="border py-3 px-5 rounded w-1/2 mb-5"
+            className="border border-gray-300  py-3 px-5 rounded w-1/2 mb-5"
           />
           {errors.todo && (
             <p className="text-red-500">This field is required</p>
           )}
           <input
             type="submit"
-            className="border py-3 hover:bg-gray-600 px-5 rounded bg-gray-400 text-white cursor-pointer"
+            className="border py-3 hover:bg-gray-600 px-5 rounded mx-1  bg-gray-500 text-white cursor-pointer"
           />
         </form>
 
