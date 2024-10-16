287e844f1292cc6299fa7cacaa4608cd578e5b51
Darshanpro123 <darshanpro187@gmail.com>
2024-10-16 11:40:07 +0530

    add message and delete functionality add

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { ToastContainer, toast, Slide } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const {
@@ -12,13 +14,19 @@ function App() {
   const [tasks, setTasks] = useState([]);
 
   const addTask = (task) => {
-    setTasks([...tasks, task.todo]);
-    console.log(task);
+    setTasks([...tasks, { id: Date.now(), todo: task.todo }]);
+    toast.success("Add data sucessFully");
   };
 
   const onSubmit = (data) => {
     addTask(data);
     reset();
+  };
+
+  const handleDelete = (id, todo) => {
+    const newTask = tasks.filter((task) => task.id !== id);
+    setTasks(newTask);
+    toast.error(`${todo} delete Sucessfully`);
   };
 
   return (
@@ -45,15 +53,28 @@ function App() {
         <ul>
           {tasks.map((task, index) => (
             <li
-              key={index}
+              key={task.id}
               className="flex justify-between border p-3 rounded-md mt-5"
             >
-              <span>{task}</span>
-              <span className="cursor-pointer">❌</span>
+              <span>{task.todo}</span>
+              <span
+                className="cursor-pointer"
+                onClick={() => handleDelete(task.id, task.todo)}
+              >
+                ❌
+              </span>
             </li>
           ))}
         </ul>
       </div>
+      <ToastContainer
+        position="bottom-right"
+        pauseOnHover={false}
+        autoClose={2000}
+        hideProgressBar={true}
+        theme="colored"
+        transition={Slide}
+      />
     </div>
   );
 }
